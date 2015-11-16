f2a2aec9564ebe55f4534f03aff4158c44e23736
icemango <icemango.hou@gmail.com>
2015-11-16 19:39:35 +0800

    techccu specified config

---
+++ techccu.js
@@ -0,0 +1,90 @@
+var slackbot = require('./lib/bot');
+var http = require('http');
+var querystring = require('querystring');
+var request = require('request');
+
+var config = {
+    server: 'irc.freenode.com',
+    nick: 'slackbot',
+    username: 'techccuslackbot',
+    token: process.env.TOKEN,
+    income_url: process.env.INCOME_URL || '',
+    outcome_token: process.env.OUTCOME_TOKEN || '',
+    channels: {
+        '#TechCCU': '#techccu_chat'
+    },
+    users: {
+    },
+    // optionals
+    floodProtection: true,
+    silent: false
+};
+
+var slackUsers = {};
+var slackChannels = {};
+function updateLists () {
+    request.get({
+        url: 'https://slack.com/api/users.list?token=' + config.token
+    }, function (error, response, body) {
+        var res = JSON.parse(body);
+        console.log('update:', new Date());
+        res.members.map(function (member) {
+            slackUsers[member.id] = member.name;
+        });
+    });
+
+    request.get({
+        url: 'https://slack.com/api/channels.list?token=' + config.token
+    }, function (error, response, body) {
+        var res = JSON.parse(body);
+        res.channels.map(function (channel) {
+            slackChannels[channel.id] = channel.name;
+        });
+    });
+
+    setTimeout(function() {
+        updateLists()
+    }, 10 * 60 * 1000);
+}
+
+updateLists();
+var slackbot = new slackbot.Bot(config);
+slackbot.listen();
+
+var server = http.createServer(function (req, res) {
+    if( req.method == 'POST') {
+        req.on('data', function(data) {
+        var payload = querystring.parse(data.toString());
+        if( payload.token == config.outcome_token && payload.user_name != 'slackbot' ) {
+            var ircMsg = "<" + payload.user_name + "> " + payload.text;
+            var channel = Object.keys(config.channels)[0];
+        //  "[-a-zA-Z0-9@:%_\+\.~#?&//=]{2,256}.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?"
+        var url = "[-a-zA-Z0-9@:%_\\+\.~#?&//=]{2,256}\\.[a-z]{2,4}\\b(\\/[-a-zA-Z0-9@:%_\\+.~#?&//=]*)?";
+        var re = new RegExp("<(" + url + ")\\|" + url + ">","gi");
+        /*
+         * Channel ID -> Channel Name
+         * Member ID -> Member Name
+         * decoed URL and remove <, >
+         */
+        ircMsg = ircMsg.replace(/<#C\w{8}>/g, function (matched) {
+          var channel_id = matched.match(/#(C\w{8})/)[1];
+          return '#' + slackChannels[channel_id];
+        }).replace(/<@U\w{8}>/g, function (matched) {
+          var member_id = matched.match(/@(U\w{8})/)[1];
+          return '@' + slackUsers[member_id];
+        }).replace(re, function (matched, link) {
+          return link.replace(/&amp;/g, '&');
+        }).replace(/&lt;/g,'<').replace(/&gt;/g, '>');
+
+        slackbot.speak(channel, ircMsg);
+        res.end('done');
+      }
+      res.end('request should not be from slackbot or must have matched token.');
+    });
+  } else {
+    res.end('recieved request (not post)');
+  }
+});
+
+server.listen(5555);
+console.log("Server running at http://localhost:5555/");
